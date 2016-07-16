4893ad403bab9b47191622519c921ea4f6919f73
Cris Silva Jr <ishiikurisu@yahoo.com>
2016-07-16 10:41:44 -0300

    Adding "no lists" screen

--- src/br/eng/crisjr/failproof/android/MainActivity.java
+++ src/br/eng/crisjr/failproof/android/MainActivity.java
@@ -3,6 +3,7 @@ package br.eng.crisjr.failproof.android;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import br.eng.crisjr.failproof.android.controller.*;
@@ -13,6 +14,7 @@ public class MainActivity
 //       implements AccessResultant
 {
     private MemoryAccess card;
+    private MainView view;
 
     /**
      * Called when the activity is first created.
@@ -21,6 +23,7 @@ public class MainActivity
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        view = new MainView();
         card = new MemoryAccess(getApplicationContext());
         updateLists();
 //        DatabaseAccess access = new DatabaseAccess();
@@ -36,13 +39,17 @@ public class MainActivity
     private void updateLists()
     {
         Context context = getApplicationContext();
+        LinearLayout layoutMain = (LinearLayout) findViewById(R.id.layoutMain);
         ScrollView scroll = (ScrollView) findViewById(R.id.scrollLists);
         card = new MemoryAccess(context);
         String[] lists = card.loadLists();
 
         if (lists == null) {
-            scroll = MainView.resetScroll(context);
+            scroll = view.resetScroll(context);
+            scroll.setId(R.id.scrollLists);
         }
         // TODO create else statement
+
+        view.replaceScroll(layoutMain, scroll);
     }
 }
--- src/br/eng/crisjr/failproof/android/view/MainView.java
+++ src/br/eng/crisjr/failproof/android/view/MainView.java
@@ -1,21 +1,56 @@
 package br.eng.crisjr.failproof.android.view;
 
 import android.content.Context;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.ScrollView;
+import android.widget.TextView;
+import br.eng.crisjr.failproof.android.R;
 
 /**
  * Monster to create views and controllers for main screen
  */
 public class MainView
 {
-    public static ScrollView resetScroll(Context context)
+    public ScrollView resetScroll(Context context)
     {
         ScrollView scroll = new ScrollView(context);
+        TextView tv = new TextView(context);
 
-        // TODO create scroll
+        tv.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                                                         ViewGroup.LayoutParams.WRAP_CONTENT));
+        tv.setTextSize(25);
+        tv.setGravity(Gravity.CENTER_HORIZONTAL);
+        tv.setText(context.getResources().getString(R.string.empty_lists));
+        ScrollView.LayoutParams lp = new ScrollView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                                                                 ViewGroup.LayoutParams.WRAP_CONTENT);
+        lp.setMargins(10, 10, 10, 10);
+        scroll.setLayoutParams(lp);
+        scroll.addView(tv);
 
         return scroll;
     }
 
     // TODO create static method for adding a list of titles to a scroll
+
+    public LinearLayout replaceScroll(LinearLayout linear, ScrollView scroll)
+    {
+        int howMany = linear.getChildCount();
+        View[] views = new View[howMany];
+
+        for (int i = 0; i < howMany; ++i)
+        {
+            views[i] = linear.getChildAt(i);
+            if (views[i].getClass() == ScrollView.class)
+                views[i] = scroll;
+        }
+
+        linear.removeAllViews();
+        for (int i = 0; i < howMany; ++i)
+            linear.addView(views[i]);
+
+        return linear;
+    }
 }
