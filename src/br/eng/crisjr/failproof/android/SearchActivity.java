fa963eecbf6bb59c77191fd1ce4df606ce40a124
Cris Silva Jr <ishiikurisu@yahoo.com>
2016-11-19 16:25:38 -0200

    Dealing with that EditText is going to be hard

--- src/br/eng/crisjr/failproof/android/SearchActivity.java
+++ src/br/eng/crisjr/failproof/android/SearchActivity.java
@@ -3,6 +3,8 @@ package br.eng.crisjr.failproof.android;
 import android.app.Activity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.Toast;
@@ -26,6 +28,7 @@ public class SearchActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search);
+        findViewById(R.id.scrollSearch).requestFocus();
 
         // download content from internet
         DatabaseAccess access = new DatabaseAccess(this);
@@ -67,6 +70,20 @@ public class SearchActivity
                 setResult(MainView.SAVE_REQUEST);
                 finish();
                 break;
+        }
+    }
+
+    /**
+     * When the edit search is clicked.
+     */
+    public void onClick_editSearch(View view) {
+        // TODO Make text default disappear when the user selects the filter edit
+        EditText editSearch = (EditText) view;
+        String expected = getResources().getString(R.string.search_edit);
+        String collected = editSearch.getText().toString();
+
+        if (expected.equals(collected)) {
+            editSearch.setText("");
         }
     }
 }
